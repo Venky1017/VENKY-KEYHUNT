1c66f90f5c3abac59d6ea6b3d37abfd871495caa
Venky1017 <venkatanaidunelli17@gmail.com>
2025-02-07 17:04:59 +0530

    Update bsgsd.h

--- bsgsd.h
+++ bsgsd.h
@@ -1,8 +1,10 @@
+// bsgsd.h - BSGSD mode header file
 #ifndef BSGSD_H
 #define BSGSD_H
 #include <stdint.h>
+#include "bloom.h"
 
 void compute_ripemd160(uint64_t key, char *output);
-void bsgsd_search(uint64_t start, uint64_t end, int k_factor);
+void bsgsd_search(uint64_t start, uint64_t end, int k_factor, bloom_filter *bf, const char *target_hash);
 
 #endif
